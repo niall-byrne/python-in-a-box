0ffdf6fa0ff31df895a76da6012f8f643d034413
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-03-29 13:42:11 -0400

    fix(SCRIPTS): host machine cli access errors

--- {{cookiecutter.project_slug}}/scripts/extras.sh
+++ {{cookiecutter.project_slug}}/scripts/extras.sh
@@ -5,15 +5,14 @@
 
 # Host machine only:  Please do not use this script inside a PIB container.
 
-set -eo pipefail
-
-GIT_ROOT="$(git rev-parse --show-toplevel)"
-export GIT_ROOT
+PIB_GIT_ROOT="$(git rev-parse --show-toplevel)"
+export PIB_GIT_ROOT
 
 pib_setup_hostmachine() {
   poetry install -E dev
-  poetry run "${GIT_ROOT}/scripts/hooks/_install.sh"
+  poetry run "${PIB_GIT_ROOT}/scripts/hooks/_install.sh"
+}
 
-  # shellcheck disable=SC2139
-  alias dev="PIB_PROJECT_NAME=\"{{cookiecutter.project_slug}}\" PIB_CONFIG_FILE_LOCATION=\"${GIT_ROOT}/assets/cli.yml\" poetry run dev"
+dev() {
+  PIB_PROJECT_NAME="{{cookiecutter.project_slug}}" PIB_CONFIG_FILE_LOCATION="${PIB_GIT_ROOT}/assets/cli.yml" poetry run dev "$@"
 }
