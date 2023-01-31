4eb5549afe7da79d8a279280420ba3ef51d35942
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-01-31 18:15:29 -0500

    docs(README): move demo image to gist

---
+++ scripts/terminalizer/render.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# scripts/terminalizer/render.sh
+# Renders the demo image for documentation.
+
+# Host machine only, requires node.js and the installation of the terminalizer application.
+
+main() {
+
+  if ! command -v terminalizer > /dev/null 2>&1; then
+    echo "You must install terminalizer to use this feature."
+    echo "https://github.com/faressoft/terminalizer"
+    echo ""
+    exit 127
+  fi
+
+  set -eo pipefail
+    terminalizer render demo.yml -o demo.gif
+  set +eo pipefail
+
+}
+
+main "$@"
